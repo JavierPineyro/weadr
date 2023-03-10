fbcac58149e89434b5fe3348ea699ac91ae59d70
JavierPineyro <javidunam22@gmail.com>
2023-03-09 23:43:15 -0300

    Refactor Header and Footer in files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import Home from '@pages/Home'
+import Home from '@/pages/Home'
 import Page404 from '@/pages/404'
 import { Route, Routes, useLocation } from 'react-router-dom'
+import Header from '@/components/header'
 
 function App() {
   const location = useLocation()
   // INSTALAR SWR
   return (
     <>
-      {/* //HEADER */}
+      <Header />
       <Routes key={location.pathname} location={location}>
         <Route path='/' element={<Home />} />
         <Route path='/search:city' element={<h3>Aboot</h3>} />
