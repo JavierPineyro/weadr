68312f8e0099b30dc6b2d7caa8b47b8787d74274
JavierPineyro <javidunam22@gmail.com>
2023-03-12 18:42:13 -0300

    Add page 404

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function NotFound() {
+  console.log('404 se veee')
   return (
-    <h1>404</h1>
+    <>
+      <h1>404</h1>
+      <Link to='/'>Volver al Inicio</Link>
+    </>
   )
 }
 
