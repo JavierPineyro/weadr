449d503bd8101e1f5747e89fb4586210a8ffc103
JavierPineyro <javidunam22@gmail.com>
2023-03-12 18:43:05 -0300

    Refactor code 🧽

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,28 +1,20 @@
 import React, { useState } from 'react'
+import Daily from '@/components/Daily'
 import { FORECAST_MOCK } from '@/mocks/data'
+import { formatDate } from '@/utils'
+
+import styles from './home.module.css'
 
 function Home() {
   const [forecast, setForecast] = useState(FORECAST_MOCK)
-
-  const formatDate = (t) => {
-    const rawDate = new Date(t * 1000) // con el epoc time transformo a date
-    const options = { dateStyle: 'full' } // opciones para mostrar la fecha
-    return new Intl.DateTimeFormat('es-AR', options).format(rawDate) // creo la fecha
-  }
+  const { forecast: hourlyForecast, ...dailyForecast } = forecast // quito lo que no uso
 
   return (
     <main>
-      <h1>{forecast.location.name}</h1>
-      <h3>{forecast.location.country}</h3>
-      <h4>{formatDate(forecast.location.localtime_epoch)}</h4>
-      <br />
-      <ul>
-        <li>{forecast.current.temp_c}°C</li>
-        <li>{forecast.current.condition.text}</li>
-        <li>{forecast.current.feelslike_c}°C sensasion termica</li>
-        <li>{forecast.current.humidity}% humedad</li>
-        <li>Viento {forecast.current.wind_kph}km/h</li>
-      </ul>
+      <h1 className={styles.title}>{forecast.location.name}</h1>
+      <h3 className={styles.country}>{forecast.location.country}</h3>
+      <h5 className={styles.date}>{formatDate(forecast.location.localtime_epoch)}</h5>
+      <Daily forecast={dailyForecast} />
       {/* ACA VIENE LA BOX CON LA HORA,TEMPERATURA Y CONDICION CHIQUITO */}
       {/* aca viene la box con la puesta del sol y salida del sol etiqueta time */}
       {/* aca se podria agregar lo mismo pero para los dos días siguientes resumidos
