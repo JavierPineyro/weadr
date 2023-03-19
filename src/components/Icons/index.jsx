a1171bdaed9f92e822c73a9f5f317647d61dc9fc
JavierPineyro <javidunam22@gmail.com>
2023-03-19 17:57:18 -0300

    Add camelcase props to ummbrella icon

--- src/components/Icons/index.jsx
+++ src/components/Icons/index.jsx
@@ -108,7 +108,7 @@ export function HumidityIcon() {
 
 export function Umbrella() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-umbrella" width="28" height="28" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-umbrella" width="28" height="28" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
       <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
       <path d="M4 12a8 8 0 0 1 16 0z"></path>
       <path d="M12 12v6a2 2 0 0 0 4 0"></path>
