b5b7a2e8f05694b839833f0d72a6b2ac0a678a7f
JavierPineyro <javidunam22@gmail.com>
2023-03-24 22:29:19 -0300

    Refactor code to components

---
+++ src/components/SearchItem/index.jsx
@@ -0,0 +1,19 @@
+import { DirectionIcon } from '@/components/Icons'
+import { Link } from 'react-router-dom'
+import styles from './searchItem.module.css'
+
+function SearchItem({ city, region, country, coords }) {
+  return (
+    <Link to={`/search/${coords}`} className={styles.article}>
+      <header className={styles.header}>
+        <DirectionIcon width={16} height={16} />
+        <p>{city}</p>
+      </header>
+      <footer className={styles.footer}>
+        {region} - {country}
+      </footer>
+    </Link>
+  )
+}
+
+export default SearchItem
